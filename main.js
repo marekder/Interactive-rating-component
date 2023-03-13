9219511ac3092737afd2eaac9eb10e006fd0ac7d
Marek Derlukiewicz <marek.derlukiewicz@gmail.com>
2023-03-13 12:14:51 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,118 @@
+import "./sass/main.scss";
+// import AOS from 'aos'
+// AOS.init()
+
+const numberOne = document.getElementById("one");
+const numberTwo = document.getElementById("two");
+const numberThree = document.getElementById("three");
+const numberFour = document.getElementById("four");
+const numberFive = document.getElementById("five");
+const submitButton = document.getElementById("submit");
+const root = document.getElementById("root");
+/* Setting the value of the variable value to 0. */
+let value = 0;
+
+/* Adding an event listener to each of the number buttons. When the button is clicked, it checks to see
+if the button is selected. If it is not selected, it sets the button to selected and sets all the
+other buttons to not selected. If the button is selected, it sets the button to not selected. */
+numberOne.addEventListener("click", () => {
+  let selected = numberOne.getAttribute("data-selected");
+  console.log(selected);
+  if (selected === "false") {
+    numberOne.setAttribute("data-selected", "true");
+    numberTwo.setAttribute("data-selected", "false");
+    numberThree.setAttribute("data-selected", "false");
+    numberFour.setAttribute("data-selected", "false");
+    numberFive.setAttribute("data-selected", "false");
+
+    value = 1;
+  } else {
+    numberOne.setAttribute("data-selected", "false");
+    value = 0;
+  }
+});
+
+numberTwo.addEventListener("click", () => {
+  let selected = numberTwo.getAttribute("data-selected");
+  console.log(selected);
+  if (selected === "false") {
+    numberTwo.setAttribute("data-selected", "true");
+    numberOne.setAttribute("data-selected", "false");
+    numberThree.setAttribute("data-selected", "false");
+    numberFour.setAttribute("data-selected", "false");
+    numberFive.setAttribute("data-selected", "false");
+
+    value = 2;
+  } else {
+    numberTwo.setAttribute("data-selected", "false");
+    value = 0;
+  }
+});
+
+numberThree.addEventListener("click", () => {
+  let selected = numberThree.getAttribute("data-selected");
+  console.log(selected);
+  if (selected === "false") {
+    numberThree.setAttribute("data-selected", "true");
+    numberOne.setAttribute("data-selected", "false");
+    numberTwo.setAttribute("data-selected", "false");
+    numberFour.setAttribute("data-selected", "false");
+    numberFive.setAttribute("data-selected", "false");
+
+    value = 3;
+  } else {
+    numberThree.setAttribute("data-selected", "false");
+    value = 0;
+  }
+});
+
+numberFour.addEventListener("click", () => {
+  let selected = numberFour.getAttribute("data-selected");
+  console.log(selected);
+  if (selected === "false") {
+    numberFour.setAttribute("data-selected", "true");
+    numberOne.setAttribute("data-selected", "false");
+    numberTwo.setAttribute("data-selected", "false");
+    numberThree.setAttribute("data-selected", "false");
+    numberFive.setAttribute("data-selected", "false");
+
+    value = 4;
+  } else {
+    numberFour.setAttribute("data-selected", "false");
+    value = 0;
+  }
+});
+
+numberFive.addEventListener("click", () => {
+  let selected = numberFive.getAttribute("data-selected");
+  console.log(selected);
+  if (selected === "false") {
+    numberFive.setAttribute("data-selected", "true");
+    numberOne.setAttribute("data-selected", "false");
+    numberTwo.setAttribute("data-selected", "false");
+    numberThree.setAttribute("data-selected", "false");
+    numberFour.setAttribute("data-selected", "false");
+
+    value = 5;
+  } else {
+    numberFive.setAttribute("data-selected", "false");
+    value = 0;
+  }
+});
+
+submitButton.addEventListener("click", () => {
+  if (value) {
+    root.innerHTML = `<article class="rating-card slideRight">
+    <img class="rating-card__illustration" src="img/illustration-thank-you.svg" alt="illustration">
+    <p class="rating-card__user-selection">You selected ${value} out of 5</p>
+    <h2 class="rating-card__title--submited">Thank you!</h2>
+    <p class="rating-card__paragraph--submited">
+    We appreciate you taking the time to give a rating. If you ever need more support, 
+    don’t hesitate to get in touch!
+  
+    </p>
+</article>`;
+  } else {
+    alert("You must select a number");
+  }
+});
